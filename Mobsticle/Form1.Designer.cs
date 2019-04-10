8d3997b6a420bc8a6cc02645bd68f58bef73720f
Greg <gregory.west@trainingcheck.com>
2019-04-10 21:13:12 +0100

    Added initial project files

---
+++ Mobsticle/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Mobsticle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtParticipants = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numMinutes = new System.Windows.Forms.NumericUpDown();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mniSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniPause = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.cboNotification = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).BeginInit();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Participants";
+            // 
+            // txtParticipants
+            // 
+            this.txtParticipants.AcceptsReturn = true;
+            this.txtParticipants.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParticipants.Location = new System.Drawing.Point(16, 30);
+            this.txtParticipants.Multiline = true;
+            this.txtParticipants.Name = "txtParticipants";
+            this.txtParticipants.Size = new System.Drawing.Size(211, 142);
+            this.txtParticipants.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minutes";
+            // 
+            // numMinutes
+            // 
+            this.numMinutes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numMinutes.Location = new System.Drawing.Point(16, 204);
+            this.numMinutes.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numMinutes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMinutes.Name = "numMinutes";
+            this.numMinutes.Size = new System.Drawing.Size(78, 20);
+            this.numMinutes.TabIndex = 3;
+            this.numMinutes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(152, 289);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Text = "Mobsticle";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniSettings,
+            this.mniPause,
+            this.mniExit,
+            this.toolStripSeparator1});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(117, 76);
+            // 
+            // mniSettings
+            // 
+            this.mniSettings.Name = "mniSettings";
+            this.mniSettings.Size = new System.Drawing.Size(116, 22);
+            this.mniSettings.Text = "Settings";
+            this.mniSettings.Click += new System.EventHandler(this.mniSettings_Click);
+            // 
+            // mniPause
+            // 
+            this.mniPause.Name = "mniPause";
+            this.mniPause.Size = new System.Drawing.Size(116, 22);
+            this.mniPause.Text = "Pause";
+            this.mniPause.Click += new System.EventHandler(this.mniPause_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(116, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // cboNotification
+            // 
+            this.cboNotification.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboNotification.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboNotification.FormattingEnabled = true;
+            this.cboNotification.Location = new System.Drawing.Point(16, 252);
+            this.cboNotification.Name = "cboNotification";
+            this.cboNotification.Size = new System.Drawing.Size(211, 21);
+            this.cboNotification.TabIndex = 5;
+            this.cboNotification.SelectedValueChanged += new System.EventHandler(this.cboNotification_SelectedValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Notification";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(239, 326);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboNotification);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.numMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtParticipants);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mobsticle";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).EndInit();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtParticipants;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numMinutes;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniSettings;
+        private System.Windows.Forms.ToolStripMenuItem mniPause;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ComboBox cboNotification;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Mobsticle/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Media;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Mobsticle
+{
+    public partial class Form1 : Form
+    {
+        private const int _sections = 24;
+        const string CURRENT = " (Current)";
+        const string NEXT = " (Next)";
+        const string PAUSE = "Pause";
+        const string ROTATE = "Rotate";
+        const string START = "Start";
+        private TimeSpan _elapsedTime = TimeSpan.Zero;
+        private Icon[] _icons16;
+        private Icon[] _icons48;
+        private bool _isPaused = false;
+        private bool _okClose = false;
+        private string _participant;
+        private List<string> _participants = new List<string>();
+        private SoundPlayer _player = new SoundPlayer();
+
+        private DateTime _startFrom;
+        private TimeSpan _totalTime = TimeSpan.FromMinutes(10);
+        public Form1()
+        {
+            InitializeComponent();
+            _icons16 = createPieIcons(16, _sections);
+            _icons48 = createPieIcons(48, _sections);
+            notifyIcon.Icon = _icons16[0];
+            notifyIcon.Visible = true;
+            Icon = _icons48[0];
+            loadNotifications();
+            Start();
+        }
+
+
+
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Hide();
+
+            _participants = split(txtParticipants.Text);
+            _totalTime = TimeSpan.FromMinutes((int)numMinutes.Value);
+
+            for (int i = contextMenuStrip.Items.Count; i > 3; i--)
+                contextMenuStrip.Items.RemoveAt(i - 1);
+            if (_participants.Count > 0)
+                contextMenuStrip.Items.Add(new ToolStripSeparator());
+            if (!_participants.Contains(_participant))
+                _participant = _participants.FirstOrDefault();
+            foreach (var participant in _participants)
+            {
+                var item = new ToolStripMenuItem(textFor(participant));
+                item.Click += participantClick;
+                item.Tag = participant;
+                contextMenuStrip.Items.Add(item);
+            }
+        }
+
+        private void Buzz()
+        {
+            timer.Stop();
+            mniPause.Text = ROTATE;
+            //SystemSounds.Exclamation.Play();            
+            _player.PlayLooping();
+        }
+
+        private void cboNotification_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cboNotification.SelectedItem != null)
+            {
+                setNotification(((Tuple<string, string>)cboNotification.SelectedItem).Item1);
+            }
+        }
+
+        private Icon[] createPieIcons(int size, int sections)
+        {
+            var icons = new List<Icon>();
+            using (var br = new SolidBrush(Color.IndianRed))
+            using (var bg = new SolidBrush(Color.PaleGreen))
+            //using (var br = new SolidBrush(Color.FromArgb(255, 255, 100, 100)))
+            //using (var bg = new SolidBrush(Color.FromArgb(200, 200, 255, 255)))
+            //using (var bb = new SolidBrush(Color.DarkGray))
+            {
+                for (int i = 0; i <= _sections; i++)
+                {
+                    using (var bitmap = new Bitmap(size, size))
+                    using (var g = Graphics.FromImage(bitmap))
+                    {
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                        float arc = (360f / sections) * i;
+                        if (i < _sections)
+                        {
+                            g.FillPie(bg, new Rectangle(0, 0, size - 1, size - 1), 270 + arc, 360 - arc);
+                        }
+                        if (i > 0)
+                        {
+                            g.FillPie(br, new Rectangle(0, 0, size - 1, size - 1), 270, arc);
+                        }
+                        var p = new GraphicsPath();
+                        //var fac = (int) (size * 0.3);
+                        //p.AddEllipse(new Rectangle(fac, fac, size - (1 + fac * 2), size - (1 + fac * 2)));
+                        //g.SetClip(p);
+                        //g.Clear(Color.Transparent);
+                        //g.ResetClip();
+                        icons.Add(Icon.FromHandle(bitmap.GetHicon()));
+                    }
+                }
+            }
+            return icons.ToArray();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!_okClose)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txtParticipants.Text = string.Join(Environment.NewLine, _participants);
+            numMinutes.Value = (int)_totalTime.TotalMinutes;
+        }
+
+        private void loadNotifications()
+        {
+            var list = new List<Tuple<string, string>>();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var prefix = assembly.GetName().Name + ".Resources.";
+            foreach (var wav in assembly.GetManifestResourceNames().Where(name => name.EndsWith(".wav")))
+            {
+                list.Add(Tuple.Create(wav, Text = wav.Substring(prefix.Length, wav.Length - (prefix.Length + 4))));
+            }
+            list.Sort((i1, i2) => i1.Item1.CompareTo(i2.Item1));
+            cboNotification.Items.AddRange(list.ToArray());
+            cboNotification.ValueMember = "Item1";
+            cboNotification.DisplayMember = "Item2";
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?", "Mobsticle", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                _okClose = true;
+                Application.Exit();
+            }
+        }
+
+        private void mniPause_Click(object sender, EventArgs e)
+        {
+            switch (mniPause.Text)
+            {
+                case PAUSE:
+                    Pause();
+                    break;
+                case START:
+                    Start();
+                    break;
+                case ROTATE:
+                    Rotate();
+                    break;
+            }
+        }
+
+        private void mniSettings_Click(object sender, EventArgs e)
+        {
+            Show();
+        }
+        private string nextParticipant()
+        {
+            return _participants[(_participants.IndexOf(_participant) + 1) % _participants.Count];
+        }
+
+        private void participantClick(object sender, EventArgs e)
+        {
+            var item = (ToolStripMenuItem)sender;
+
+        }
+
+        private void Pause()
+        {
+            var now = DateTime.Now;
+            timer.Stop();
+            mniPause.Text = START;
+            _elapsedTime += now - _startFrom;
+        }
+
+        private void Rotate()
+        {
+            _player.Stop();
+            var now = DateTime.Now;
+            mniPause.Text = PAUSE;
+            _elapsedTime = TimeSpan.Zero;
+            _startFrom = now;
+            timer.Start();
+
+            if (_participants.Count > 0)
+            {
+                contextMenuStrip.Items.Cast<ToolStripItem>().SingleOrDefault(i => (string)i.Tag == _participant).Text = _participant;
+                _participant = nextParticipant();
+                contextMenuStrip.Items.Cast<ToolStripItem>().SingleOrDefault(i => (string)i.Tag == _participant).Text = _participant + CURRENT;
+                contextMenuStrip.Items.Cast<ToolStripItem>().SingleOrDefault(i => (string)i.Tag == nextParticipant()).Text = nextParticipant() + NEXT;
+            }
+        }
+
+        private void setNotification(string name)
+        {
+            if (name != null)
+            {
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                _player.Stream = assembly.GetManifestResourceStream(name);
+            }
+        }
+        private List<string> split(string input)
+        {
+            return Regex.Split(input, Environment.NewLine).Select(s => s.Trim()).Where(x => !string.IsNullOrWhiteSpace(x) && Regex.IsMatch(x, "[\\w\\s\\d]{1,30}")).Distinct().ToList();
+        }
+
+        private void Start()
+        {
+            var now = DateTime.Now;
+            timer.Start();
+            mniPause.Text = PAUSE;
+            _startFrom = now;
+        }
+
+        private string textFor(string participant)
+        {
+            if (_participant == participant)
+                return participant + CURRENT;
+            if (participant == nextParticipant())
+                return participant + NEXT;
+            return participant;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            _elapsedTime += now - _startFrom;
+            _startFrom = now;
+            if (_elapsedTime > _totalTime)
+                _elapsedTime = _totalTime;
+
+            var percentage = _elapsedTime.TotalMilliseconds / _totalTime.TotalMilliseconds;
+            var section = (int)(_sections * percentage);
+
+            notifyIcon.Icon = _icons16[section];
+            Icon = _icons48[section];
+
+            if (_elapsedTime >= _totalTime)
+                Buzz();
+        }
+    }
+}
